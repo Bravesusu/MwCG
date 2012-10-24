cfe0bf536a19f98656a53a93d6cfc0ab661e9edf
AKFish <akfish@gmail.com>
2012-10-24 19:09:07 +0800

    Initial.

---
+++ MwCG/MwCGView.cpp
@@ -0,0 +1,137 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://go.microsoft.com/fwlink/?LinkId=238214.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// MwCGView.cpp : implementation of the CMwCGView class
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "MwCG.h"
+#endif
+
+#include "MwCGDoc.h"
+#include "MwCGView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMwCGView
+
+IMPLEMENT_DYNCREATE(CMwCGView, CView)
+
+BEGIN_MESSAGE_MAP(CMwCGView, CView)
+	// Standard printing commands
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CMwCGView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CMwCGView construction/destruction
+
+CMwCGView::CMwCGView()
+{
+	// TODO: add construction code here
+
+}
+
+CMwCGView::~CMwCGView()
+{
+}
+
+BOOL CMwCGView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: Modify the Window class or styles here by modifying
+	//  the CREATESTRUCT cs
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMwCGView drawing
+
+void CMwCGView::OnDraw(CDC* /*pDC*/)
+{
+	CMwCGDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: add draw code for native data here
+}
+
+
+// CMwCGView printing
+
+
+void CMwCGView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CMwCGView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// default preparation
+	return DoPreparePrinting(pInfo);
+}
+
+void CMwCGView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add extra initialization before printing
+}
+
+void CMwCGView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: add cleanup after printing
+}
+
+void CMwCGView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CMwCGView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CMwCGView diagnostics
+
+#ifdef _DEBUG
+void CMwCGView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMwCGView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMwCGDoc* CMwCGView::GetDocument() const // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMwCGDoc)));
+	return (CMwCGDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMwCGView message handlers
