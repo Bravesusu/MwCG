f3f69069695a8bae164477ee92db50c0a2c44939
AKFish <akfish@gmail.com>
2012-11-06 13:56:32 +0800

    Empty GlCmd class

---
+++ MwCG/GlCmd.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "GlCmd.h"
+
+using namespace mw;
+
+GlCmd::GlCmd(void)
+{
+}
+
+
+GlCmd::~GlCmd(void)
+{
+}
---
+++ MwCG/GlCmd.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "afx.h"
+
+namespace mw {
+	class GlCmd :
+		public CObject
+	{
+	public:
+		GlCmd(void);
+		~GlCmd(void);
+	};
+};
+
