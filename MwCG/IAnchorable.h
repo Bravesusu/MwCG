1c3829be0c7d55fb3c000057e6d9cef1327f715c
AKFish <akfish@gmail.com>
2012-11-26 21:32:31 +0800

    Clear unused data fields in IOperation

--- MwCG/IAnchorable.h
+++ MwCG/IAnchorable.h
@@ -4,6 +4,10 @@ namespace mw
 {
 	class IAnchorable
 	{
+	public:
+		virtual int anchor_count() const = 0;
+		virtual void set_anchor(int index, const Vector2& localPos) = 0;
+		virtual Vector2 anchor(int index) const = 0;
 	};
 }
 
